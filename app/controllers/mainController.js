ab99d5336f7a0beef743ec455ea1628d0cdb221b
papoose974 <alex-97.4@hotmail.fr>
2024-06-06 23:02:36 +0200

    j4

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -5,7 +5,7 @@ maintController = {
     homePage: async (req, res) => {
         try {
         const coffees = await dataMapper.getAllCoffee();    
-        res.render ('accueil', { coffee });
+        res.render ('accueil', { coffees });
         } catch (error) {
         console.log(error);
         res.status(500).send('Erreur 500 - Le serveur ne répond pas ...');
@@ -14,19 +14,19 @@ maintController = {
 
     cataloguePage: async (req, res) => {
         try {
-        const coffee = await dataMapper.getAllCoffee();    
-        res.render ('catalogue', { coffee });
+        const coffees = await dataMapper.getAllCoffee();    
+        res.render ('catalogue', { coffees });
         } catch (error) {
         console.log(error);
         res.status(500).send('Erreur 500 - Le serveur ne répond pas ...');
         }
     },
-                
+               
     articlePage: async (req, res) => {
         try {
         const id = parseInt(req.params.id, 10);
-        const coffee = await dataMapper.getAllCoffee(id);    
-        res.render ('article', { coffee });
+        const coffees = await dataMapper.getAllCoffee();    
+        res.render ('article', { coffees });
         } catch (error) {
         console.log(error);
         res.status(500).send('Erreur 500 - Le serveur ne répond pas ...');
--- app/dataMapper.js
+++ app/dataMapper.js
@@ -7,17 +7,12 @@ const dataMapper = {
         const result = await client.query(query);
         return result.rows;
         },
-        
-        
-        getDivCoffee: async (id) => {
-            const query = 'SELECT name, price, description FROM ocoffee WHERE id = $1';
-            const values = [id];
-            const result = await client.query(query, values);
-            console.log('resultat', result.rows[0]);
-        return result.rows[0];
-    },   
+    getCoffeebyID: async (id) => {
+        const query = 'SELECT * FROM your_table_name WHERE id = $1';
+        const values = [id];
+        const result = await client.query(query, values);
+    return result.rows[0];
+    }
 };
-
+        
 module.exports = dataMapper;
-
-
